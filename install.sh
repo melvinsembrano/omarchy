fb92ba157f1314ff8fff0eb9257e3909290f77d0
Melvin Sembrano <melv@hey.com>
2025-08-15 12:09:30 +1000

    Update install.sh
    Remove unecessary installations.

--- install.sh
+++ install.sh
@@ -69,8 +69,8 @@ source $OMARCHY_INSTALL/desktop/printer.sh
 # Apps
 show_logo expand
 show_subtext "Installing default applications [4/5]"
-source $OMARCHY_INSTALL/apps/webapps.sh
-source $OMARCHY_INSTALL/apps/xtras.sh
+# source $OMARCHY_INSTALL/apps/webapps.sh
+# source $OMARCHY_INSTALL/apps/xtras.sh
 source $OMARCHY_INSTALL/apps/mimetypes.sh
 
 # Updates
